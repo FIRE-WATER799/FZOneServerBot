a905dbd2ebb52963f88f2c135e03f070e72df5bd
FIRE-WATER799 <haywood2008918@outlook.com>
2023-03-06 14:25:56 -0600

    should be finished

--- src/cogs/FzCommands1.py
+++ src/cogs/FzCommands1.py
@@ -11,6 +11,12 @@ from src.extras.posts import fz_login_post, fz_start_post
 from src.extras.settingsSelector import save
 from src.extras.tokenSelector import TokenSelectorView
 
+def write_json(new_data, filename='data.json'):
+    with open(filename,'r+') as file:
+        file_data = json.load(file)
+        file_data.update(new_data)
+        file.seek(0)
+        json.dump(file_data, file, indent = 4)
 
 class FzCommands(commands.Cog):
     """Class for the Factorio Zone cog"""
@@ -48,6 +54,43 @@ class FzCommands(commands.Cog):
                 chan = self.bot.get_channel(chanelid)
                 await chan.send(f"Server started at `{ip}`\nLaunch Id: {launchId}")
                 self.bot.active_sockets.append((ctx.guild.id, socket, socket_task))
+    
+    @app_commands.command(name="connect", description="Connect to Factorio Zone Servers")
+    @app_commands.guilds(1053562951497420871)
+    async def _settings(self, ctx:discord.Interaction):
+        await ctx.response.defer(ephemeral=True)
+        data = json.load(open('data.json'))
+        token = data[ctx.guild.id]['token']
+        channelid = data[ctx.guild.id]['channelid']
+        for guild_id, socket, socket_task in self.bot.active_sockets:
+            if guild_id == ctx.guild.id:
+                socket = FzSocket(ctx.guild.id)
+                socket_task = asyncio.create_task(socket.connect())
+                await ctx.edit_original_message(content="Let me pull up the settings information", view=None)
+                respo = await fz_login_post(await socket.get_secret(), token)
+                if respo is not None:
+                    regions = await socket.get_regions()
+                    saves = await socket.get_saves()
+                    versions = await socket.get_versions()
+                    settingsView = SettingsSelectorView(regions, saves, versions)
+                    await ctx.edit_original_message(content=f"Select the settings", view=settingsView)
+                    await settingsView.wait()
+                    data = {
+                        f"{ctx.guild.id}": {
+                            "token": token
+                            "channelid": channelid
+                            "save": str(settingsView.selected_save),
+                            "version": str(settingsView.selected_version),
+                            "region": str(settingsView.selected_region)
+                        }
+                    }
+                    write_json(data)
+                    await ctx.edit_original_message(content="Settings saved")
+                socket_task.cancel()
+
+
+
+
 
 async def setup(bot):
     await bot.add_cog(FzCommands(bot))
